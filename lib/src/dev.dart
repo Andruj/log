1e579897ab2f0cb7d721d6bbaedf39c1e3c9267e
Andrew Peterson <Andruj@users.noreply.github.com>
2016-03-04 12:31:31 -0800

    Updated better usage instead of ternary operators.

--- lib/src/dev.dart
+++ lib/src/dev.dart
@@ -6,9 +6,9 @@ abstract class Dev {
 
   static enable() => enabled = true;
   static disable() => enabled = false;
-
-  static message(text) => enabled ? Log.message('$prefix$text') : null;
-  static error(text) => enabled ? Log.error('$prefix$text') : null;
-  static warning(text) => enabled ? Log.warning('$prefix$text') : null;
-  static success(text) => enabled ? Log.success('$prefix$text') : null;
+  // Only log if enabled is true.
+  static message(text) => enabled && Log.message('$prefix$text');
+  static error(text) => enabled && Log.error('$prefix$text');
+  static warning(text) => enabled && Log.warning('$prefix$text');
+  static success(text) => enabled && Log.success('$prefix$text');
 }
